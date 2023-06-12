729b11c50bb1f1f9fb840f21131422e43e06f07c
AJAYI MICHAEL <MICHAEL@MACBOOKs-MacBook-Pro.local>
2023-06-12 21:57:31 +0100

    CICD Pipeline file added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(DefaultAdmin::class);
         $this->call(EventSeeder::class);
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
 
     }
 }
